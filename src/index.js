c296386b5c8b9b046d7d90c5ab9c3ed74746aadc
Mitra Tabatabaei <mi.tabatabaei@deltaban.local>
2022-02-27 12:41:41 +0330

    add minicssExtarctPlugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import some from './some.js';
-import './style.css';
+import './style.scss';
 some(3, 5);
 console.log(some(3, 5));
 console.log('mmmm')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
@@ -8,14 +9,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[hash].[filename].css'
+        })
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
     }
